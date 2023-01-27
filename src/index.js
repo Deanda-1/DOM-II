169cdbdc85a8cd420cc103ab1cc3812a90a6ad26
DJ1 <jimmynoble604@gmail.com>
2023-01-27 14:58:09 -0500

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,72 @@
 import './less/index.less'
 
 // Your code goes here!
+ const mouseover = document.querySelector('.mouseover')
+   mouseover.addEventListenerAll(function(nav) {
+    MouseEvent.call.array.forEach(element => {
+        'The ${nav-item} opens up when the mouse hovers over the ${nav-item}'; 
+    });
+   })   
+ const keydown = document.querySelector('.keydown')
+    keydown.addEventListener(function(nav) {
+        keyDownEvent.call.onclick.MouseEvent.forEach(element => {
+            'The ${nav-item} will engage when the mouse clicks on the ${nav-item}';
+        });
+    })
+
+ const wheel = document.querySelector('.wheel')
+    wheel.addEventListener(function(nav) {
+        WheelEvent.call.MouseEvent.forEach(navItem => {
+            'Scroll through each ${nav-item} when the mouse is clicked on it.';
+        });
+    })
+
+ const load = document.querySelector('.load')
+    load.addEventListener(function(nav) {
+        loadOptions.onclick.MouseEvent.call.forEach(navItem => {
+            'Upload ${nav-item} when clicked on.';
+        });
+    })
+
+ const foucs = document.querySelector('.focus')
+    foucs.addEventListener(function(navItem) {
+        onclick.MouseEvent.call.forEach(navItem => {
+            'Deal with the ${navItem} until finished.';
+        });
+    })
+
+ const resize = document.querySelector('.resize')
+    resize.addEventListener(function(h1, h2, p) {
+        element.addEventListener('click', (event, h1) => { event.target.style.fontSize = '40px'; });
+        element.addEventListener('click', (event,h2) => { event.target.style.fontSize = '20px'; });
+        element.addEventListener('click', (event, p) => { event.target.style.fontSize = '10px'; });
+    })
+
+ const scroll = document.querySelector('.scroll')
+    scroll.addEventListener(function(navItem) {
+        scrollTo.name.MouseEvent.call.forEach(navItem => {
+            '${navItem} can be studied and read carfully.';
+        });
+    })
+
+ const select = document.querySelector('.select')
+    select.addEventListener(function(navItem) {
+        Selection.call.navItem.MouseEvent.element.forEach(navItem => {
+            'Click on the ${select} section to look at and study.';
+        });
+    })
+
+ const dbclick = document.querySelector('.dbclick')
+    dbclick.addEventListener(function(clickMe) {
+        object.ondblclick = function() { clickMe };
+        object.addEventListener("dblclick", clickMe);   
+    })
+
+ const dragDrop = document.querySelector('.dragDrop')
+    dragDrop.addEventListener(function(img) {
+        img.addEventListenerAll.array.call.MouseEvent.forEach(img => {
+            '${img} add by ${dragDrop}';
+        });
+    })
+
+
